e2b457f02c209d8eb62271969fbb1d4d2bf39033
Saumyak Raj <rajsaumyak@gmail.com>
2022-04-06 23:49:01 +0530

    2nd html added

--- retrieval_sys/urls.py
+++ retrieval_sys/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 from retrieval_sys import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('upload_image', views.upload_image, name='upload_image'),
+    path('', views.upload_image, name='upload_image'),
 ]
