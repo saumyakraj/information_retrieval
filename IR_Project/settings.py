cf8be7fcde0331aa238cc9373a4096dc4393db25
Saumyak Raj <rajsaumyak@gmail.com>
2022-04-08 23:11:13 +0530

    ficed error

--- IR_Project/settings.py
+++ IR_Project/settings.py
@@ -119,14 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/Data/images_test/'
-STATIC_ROOT = '/Data/images_test/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
